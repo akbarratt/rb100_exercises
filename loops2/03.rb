0563fcd0fa6e00a88345547affefa94c8abb2cc8
AK Barratt <andria@akbarratt.com>
2021-01-26 17:18:11 -0500

    Continue work on Loops 2 exercises.

--- loops2/03.rb
+++ loops2/03.rb
@@ -1,13 +1,12 @@
 # In progress.
 
-# The variable given to us, process_the_loop, will be assigned randomly to either true or false. Our goal here is to run the loop only if process_the_loop evaluates to true.
-
 process_the_loop = [true, false].sample
 
-loop do
-  if process_the_loop
+if process_the_loop
+  loop do
     puts "The loop was processed!"
-  else
-    puts "The loop wasn't processed!"
+    break
   end
+else
+  puts "The loop wasn't processed!"
 end
